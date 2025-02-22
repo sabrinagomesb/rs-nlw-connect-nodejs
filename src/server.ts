35090d44073ba6012a32eb9b01fb774ba5cbad7f
Sabrina Gomes <sabrinagbdev@gmail.com>
2025-02-22 18:11:59 -0300

    feat: add function and route to get subscriber invites count

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import {
 import { env } from './env'
 import { accessInviteLinkRoute } from './routes/access-invite-link-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -39,6 +40,7 @@ app.register(fastifySwaggerUi, {
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
 
 app.get('/hello', () => {
   return 'Hello, world!'
